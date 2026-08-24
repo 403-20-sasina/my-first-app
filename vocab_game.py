90dbff8dca2bc53352d7a4696fc9237626e8f404
iloveAnton <54592@yupparaj.ac.th>
2026-08-24 08:58:33 +0700

    Fix text input value assignments in vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -72,19 +72,19 @@ st.divider()
 
 ans1 = st.text_input(
     "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
-    "value = st.session_state.ans1_val,"
+    value = st.session_state.ans1_val,
 )
 ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
-    "value = st.session_state.ans2_val,"
+    value = st.session_state.ans2_val,
 )
 ans3 = st.text_input(
     "ข้อ 3: A five-member K-pop idol group from HYPE Entertrainment `C_ _ t i _`. 👓",
-    "value = st.session_state.ans3_val,"
+    value = st.session_state.ans3_val,
 )
 ans4 = st.text_input(
     "ข้อ 4: A four-member K-pop idol group from J-park Entertrainment `L n _ s _ o _`. 📱",
-    "value = st.session_state.ans4_val,"
+    value = st.session_state.ans4_val,
 )
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
